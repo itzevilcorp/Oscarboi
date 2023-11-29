07775e95636b64adf580db8035d96a56a8941797
balpreet.balli <Balpreet.Singh@triosstudent.com>
2023-11-29 09:44:45 -0500

    first

---
+++ Program.fs
@@ -0,0 +1,21 @@
+﻿type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+
+type Director = { Name: string; Movies: int }
+
+
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    imbd: float
+}
+
+
